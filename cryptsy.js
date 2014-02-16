cdca2394b8fde72a48382ca78b7f75ead45e9217
Konstantinos Vaggelakos <kozze89@gmail.com>
2014-02-15 18:31:08 -0800

    Refactoring, making it more node.js'y

--- cryptsy.js
+++ cryptsy.js
@@ -1,152 +1,200 @@
-var stringify = require("querystring").stringify,
-    hmac = require("crypto").createHmac,
-    request = require("request"),
-    publicMethods = ['marketdata','marketdatav2','orderdata','singleorderdata','singlemarketdata'];
+var stringify = require('querystring').stringify,
+  hmac = require('crypto').createHmac,
+  request = require('request'),
+  publicMethods = ['marketdata','marketdatav2','orderdata',
+    'singleorderdata','singlemarketdata'];
 
+/**
+ * CryptsyClient constructor. Needs keys to make private requests
+ * @param {String} key Public key from cryptsy
+ * @param {String} secret Private key from cryptsy
+ */
 function CryptsyClient(key, secret) {
-  var self = this;
-  self.key = key;
-  self.secret = secret;
+  this.key = key;
+  this.secret = secret;
+}
 
-  function api_query(method, callback, args)
-  {
-    var args_tmp = {};
+CryptsyClient.prototype.apiQuery = function(method, callback, args) {
+  var argsTmp = {};
 
-    for(i in args)
-      if(args[i])
-        args_tmp[i] = args[i];
-
-    args = args_tmp;
-
-    var options = {
-      uri: 'https://api.cryptsy.com/api',
-      agent: false,
-      method: 'POST',
-      headers: {
-        "User-Agent": "Mozilla/4.0 (compatible; Cryptsy API node client)",
-        "Content-type": "application/x-www-form-urlencoded"
-      }
-    };
-    args.method = method;
-    if(publicMethods.indexOf(method) > -1)
-    {
-      options.method = 'GET';
-      options.uri = 'http://pubapi.cryptsy.com/api.php?' + stringify(args); 
+  for(var i in args) {
+    if(args[i]) {
+      argsTmp[i] = args[i];
     }
-    else
-    {
-      if (!self.key || !self.secret) {
-        throw new Error("Must provide key and secret to make this API request.");
-      }
-      else
-      {
-        args.nonce = new Date().getTime();
-        var message = stringify(args);
-        var signed_message = new hmac("sha512", self.secret);
-        signed_message.update(message);
-        options.headers.Key = self.key;
-        options.headers.Sign = signed_message.digest('hex');
-        options.body = message;
-      }
-    }
-    request(options, function(err, res, body) {
-      var response = JSON.parse(body);
-      if(parseInt(response.success) === 1 && typeof callback == typeof Function)
-        callback(response.return);
-      else if(response.error)
-        throw new Error(response.error);
-    });
   }
 
-  // This function gets the market id for a market in the format 'LTCBTC'
-  self.getmarketid = function(marketname, callback) {
-    if(!self.markets || !self.markets.length)
-    {
-      self.getmarkets(function() {
-        callback(self.markets[marketname]);
-      });
+  args = argsTmp;
+
+  var options = {
+    uri: 'https://api.cryptsy.com/api',
+    json: true,
+    agent: false,
+    method: 'POST',
+    headers: {
+      'User-Agent': 'Mozilla/4.0 (compatible; Cryptsy API node client)',
+      'Content-type': 'application/x-www-form-urlencoded'
     }
-    else
-      callback(self.markets[marketname]);
   };
 
-  // Old API method
-  self.marketdata = function(callback) {
-    api_query('marketdata', callback);
-  }
+  args.method = method;
 
-  // New API method
-  self.marketdatav2 = function(callback) {
-    api_query('marketdatav2', callback);
-  }
+  if(publicMethods.indexOf(method) > -1) {
+    options.method = 'GET';
+    options.uri = 'http://pubapi.cryptsy.com/api.php?' + stringify(args);
+  } else {
+    if (!this.key || !this.secret) {
+      throw new Error('Must provide key and secret to make this API request');
+    } else {
+      args.nonce = new Date().getTime();
+      var message = stringify(args);
+      var signedMessage = new hmac('sha512', this.secret);
+      signedMessage.update(message);
 
-  self.singlemarketdata = function(marketid, callback) {
-    api_query('singlemarketdata', callback, { marketid: marketid });
-  }
-
-  self.orderdata = function(callback) {
-    api_query('orderdata', callback);
-  }
-
-  self.singleorderdata = function(marketid, callback) {
-    api_query('singleorderdata', callback, { marketid: marketid });
-  }
-
-  self.getinfo = function(callback) {
-    api_query("getinfo", callback);
-  }
-
-  self.getmarkets = function(callback) {
-    callback2 = function(markets) {
-      self.markets = {};
-      for(var i in markets)
-      {
-        self.markets[markets[i].primary_currency_code + markets[i].secondary_currency_code] = markets[i].marketid;
-      }
-      callback(markets);
+      options.headers.Key = this.key;
+      options.headers.Sign = signedMessage.digest('hex');
+      options.body = message;
     }
-    api_query('getmarkets', callback2);
   }
 
-  self.mytransactions = function(callback) {
-    api_query('mytransactions', callback);
-  }
+  request(options, function(err, res, body) {
+    var error = function(msg) {
+      return callback('There was an error in the reqeust: '+ msg);
+    };
 
-  self.markettrades = function(marketid, callback) {
-    api_query('markettrades', callback, { marketid: marketid });
-  }
+    // Look for HTTP errors
+    if (err) {
+      return error(err);
+    }
 
-  self.marketorders = function(marketid, callback) {
-    api_query('marketorders', callback, { marketid: marketid });
-  }
+    // Check cryptsy errors
+    if (parseInt(body.success) === 1 &&
+        typeof callback === typeof Function) {
+      callback(body.return);
+    } else if (body.error) {
+      return error(body.error);
+    }
+  });
+};
 
-  self.mytrades = function(marketid, limit, callback) {
-    api_query('mytrades', callback, { marketid: marketid, limit: limit })
+/**
+ * This function gets the market id for a market in the format 'LTCBTC'
+ */
+CryptsyClient.prototype.getMarketId = function(marketname, callback) {
+  if(!this.markets || !this.markets.length) {
+    this.getmarkets(function() {
+      return callback(this.markets[marketname]);
+    });
+  } else {
+    return callback(this.markets[marketname]);
   }
+};
 
-  self.allmytrades = function(callback) {
-    api_query('allmytrades', callback);
-  }
+/**
+ * Old API method
+ */
+CryptsyClient.prototype.marketdata = function(callback) {
+  this.apiQuery('marketdata', callback);
+};
 
-  self.myorders = function(marketid, callback) {
-    api_query('myorders', callback, { marketid: marketid });
-  }
+/**
+ * New API method
+ */
+CryptsyClient.prototype.marketdatav2 = function(callback) {
+  this.apiQuery('marketdatav2', callback);
+};
 
-  self.allmyorders = function(callback) {
-    api_query('allmyorders', callback);
-  }
+CryptsyClient.prototype.singlemarketdata = function(marketid, callback) {
+  this.apiQuery('singlemarketdata', callback, {
+    marketid: marketid
+  });
+};
 
-  self.createorder = function(marketid, ordertype, quantity, price, callback) {
-    api_query('createorder', callback, { marketid: marketid, ordertype: ordertype, quantity: quantity, price: price });
-  }
+CryptsyClient.prototype.orderdata = function(callback) {
+  this.apiQuery('orderdata', callback);
+};
 
-  self.cancelorder = function(orderid, callback) {
-    api_query('cancelorder', callback, { orderid: orderid });
-  }
+CryptsyClient.prototype.singleorderdata = function(marketid, callback) {
+  this.apiQuery('singleorderdata', callback, {
+    marketid: marketid
+  });
+};
 
-  self.calculatefees = function(ordertype, quantity, price, callback) {
-    api_query('calculatefees', callback, { ordertype: ordertype, quantity: quantity, price: price });
-  }
-}
+CryptsyClient.prototype.getinfo = function(callback) {
+  this.apiQuery('getinfo', callback);
+};
+
+CryptsyClient.prototype.getmarkets = function(callback) {
+  var callback2 = function(markets) {
+    this.markets = {};
+    for(var i in markets) {
+      this.markets[markets[i].primary_currency_code +
+        markets[i].secondary_currency_code] = markets[i].marketid;
+    }
+    callback(markets);
+  };
+
+  this.apiQuery('getmarkets', callback2);
+};
+
+CryptsyClient.prototype.mytransactions = function(callback) {
+  this.apiQuery('mytransactions', callback);
+};
+
+CryptsyClient.prototype.markettrades = function(marketid, callback) {
+  this.apiQuery('markettrades', callback, {
+    marketid: marketid
+  });
+};
+
+CryptsyClient.prototype.marketorders = function(marketid, callback) {
+  this.apiQuery('marketorders', callback, {
+    marketid: marketid
+  });
+};
+
+CryptsyClient.prototype.mytrades = function(marketid, limit, callback) {
+  this.apiQuery('mytrades', callback, {
+    marketid: marketid,
+    limit: limit
+  });
+};
+
+CryptsyClient.prototype.allmytrades = function(callback) {
+  this.apiQuery('allmytrades', callback);
+};
+
+CryptsyClient.prototype.myorders = function(marketid, callback) {
+  this.apiQuery('myorders', callback, {
+    marketid: marketid
+  });
+};
+
+CryptsyClient.prototype.allmyorders = function(callback) {
+  this.apiQuery('allmyorders', callback);
+};
+
+CryptsyClient.prototype.createorder =
+  function(marketid, ordertype, quantity, price, callback) {
+  this.apiQuery('createorder', callback, {
+    marketid: marketid,
+    ordertype: ordertype,
+    quantity: quantity,
+    price: price
+  });
+};
+
+CryptsyClient.prototype.cancelorder = function(orderid, callback) {
+  this.apiQuery('cancelorder', callback, { orderid: orderid });
+};
+
+CryptsyClient.prototype.calculatefees =
+  function(ordertype, quantity, price, callback) {
+  this.apiQuery('calculatefees', callback, {
+    ordertype: ordertype,
+    quantity: quantity,
+    price: price
+  });
+};
+
 
 module.exports = CryptsyClient;
